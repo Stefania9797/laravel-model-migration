e82f630456c415eca1f26b02fca38ff0021da003
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-13 16:06:21 +0200

    aggiunto sass

--- viaggi/resources/views/home.blade.php
+++ viaggi/resources/views/home.blade.php
@@ -2,12 +2,16 @@
 @section('title','HomePage')
 @section('content')
 <h1>I Nostri Pacchetti</h1>
+<div class="viaggi">
     @foreach ($viaggi as $viaggio)
-    <h2>{{$viaggio->nome_pacchetto}}</h2>
-    <p>{{$viaggio->descrizione}}</p>
-    <p><strong>Luogo di partenza: {{$viaggio->luogo_di_partenza}}</strong></p>
-    <p><strong>Data di partenza: {{$viaggio->data_di_partenza}}</strong></p>
-    <p><strong>Durata Viaggio: {{$viaggio->durata_giorni_di_viaggio}}</strong></p>
-    <p><strong>Prezzo: {{$viaggio->price}} EURO</strong></p>
+    <div class="viaggio">
+        <h2>{{$viaggio->nome_pacchetto}}</h2>
+        <p>{{$viaggio->descrizione}}</p>
+        <p><strong>Luogo di partenza: </strong>{{$viaggio->luogo_di_partenza}}</p>
+        <p><strong>Data di partenza:</strong>{{$viaggio->data_di_partenza}}</p>
+        <p><strong>Durata Viaggio:</strong> {{$viaggio->durata_giorni_di_viaggio}} Giorni</p>
+        <p><strong>Prezzo:</strong> {{$viaggio->price}} EURO</p>
+    </div>
     @endforeach
+</div>
 @endsection
